b37bdafcf58912835fe58f29465698435b6d9012
MikeChaiwat <mrchaiwat2542@gmail.com>
2020-06-25 20:56:58 +0700

    first commit

---
+++ src/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpg",
+    name: "new yrok bridge tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpg",
+    name: "paris lights tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpg",
+    name: "london royal palace tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpg",
+    name: "tokyo sushi tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  }
+];
